1ee9d0480d9d02860e21261be52fb84bfb5aec20
Masahiro Kakizaki <mkakiz@Masahiros-MacBook-Air.local>
2020-02-25 06:36:37 -0500

    comment validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,8 @@ class CommentsController < ApplicationController
       flash[:notice] = 'Comment was posted'
       redirect_to post_path(comment.post_id)
     else
-      redirect_to :back, flash: {
+      # redirect_to :back, flash: {
+      redirect_to post_path(comment.post_id), flash: {
         comment: comment,
         error_messages: comment.errors.full_messages
       }
