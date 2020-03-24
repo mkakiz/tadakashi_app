5d03d57d876cb7ee23e9222909ccdcd1fabaa1c3
mkakiz <52940447+mkakiz@users.noreply.github.com>
2020-03-24 19:30:42 -0400

    change from sqlite3 to mysql2

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2020_02_27_014743) do
 
-  create_table "comments", force: :cascade do |t|
-    t.integer "post_id"
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "post_id"
     t.string "name", null: false
     t.text "comment", null: false
     t.integer "user_id", null: false
@@ -22,14 +22,14 @@ ActiveRecord::Schema.define(version: 2020_02_27_014743) do
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
-  create_table "likes", force: :cascade do |t|
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_02_27_014743) do
     t.string "post_image_name"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
@@ -46,4 +46,5 @@ ActiveRecord::Schema.define(version: 2020_02_27_014743) do
     t.string "password_digest"
   end
 
+  add_foreign_key "comments", "posts"
 end
