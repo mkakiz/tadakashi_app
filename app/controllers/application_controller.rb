3520fabb00c0a77fa672ff7b8d6b97e33e835157
mkakiz <masahiro.kakizaki@gmail.com>
2020-05-15 15:16:30 -0400

    rails test, get about2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user
 
   def set_current_user
-    @current_user = User.find_by(id: session[:user_id])
+    # @current_user = User.find_by(id: session[:user_id])
+    @current_user = User.find_by(id: session[:user_id]) if session[:user_id].present?
   end
 
   def authenticate_user
