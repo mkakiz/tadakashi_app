78dd40c3d75f0a134a1cce307aa61cb236e89b1b
mkakiz <masahiro.kakizaki@gmail.com>
2020-05-21 14:17:07 -0400

    rails test, get post, login, password_digest

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,11 +1,12 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  post "/login", params: {user: {
-    email:    users(:one).email,
-    password: users(:one).password
-  }}
   test "should get post" do
+    post "/login", params: {user: {
+     name: users(:one).name,
+     email:  users(:one).email,
+     password_digest: users(:one).password_digest
+    }}
     get "/posts/:id"
     assert_response :success
   end
