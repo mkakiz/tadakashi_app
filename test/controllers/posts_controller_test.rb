1d38634f251df27ec89c0f84e7dbfc353208158c
mkakiz <masahiro.kakizaki@gmail.com>
2020-05-25 17:38:35 -0400

    rails test, get post, login, image_name

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,10 +2,11 @@ require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   test "should get post" do
-    post "/login", params: {user: {
-      email: 'testuser@example.com',
-      password: '123456'
-    }}
+    post "/login", params: {
+      email: 'aaa@bbb.com',
+      password: '123456',
+      image_name: "default_user.jpg"
+    }
     get "/posts/index"
     assert_response :success
   end
