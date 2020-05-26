c91ee8f2afcb38f570dedbf998431184aca3d2fb
mkakiz <masahiro.kakizaki@gmail.com>
2020-05-26 13:07:52 -0400

    rails test, signup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,27 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get "/signup"
+    assert_no_difference 'User.count' do
+      post "/users/create", params: {
+        name: 'test',
+        email: 'testuser@example',
+        password: '123456',
+      }
+    end
+  end
+
+  test "valid signup information" do
+    get "/signup"
+    assert_difference 'User.count', 1 do
+      post "/users/create", params: {
+        name: 'testz',
+        email: 'testuserz@example.com',
+        password: '1234567',
+      }
+    end
+  end
+
 
 end
