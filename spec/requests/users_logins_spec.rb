8fd1bafd06eb9438abbcfd197bc81abe218341f4
mkakiz <masahiro.kakizaki@gmail.com>
2020-06-16 22:09:40 -0400

    rspec, user edit

--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -9,9 +9,10 @@ RSpec.describe "SignUp & Logins", type: :request do
         password: user.password,
       }
       expect(user).to be_valid
-      expect(response).to have_http_status(:redirect)
-      get posts_index_path
-      expect(response).to have_http_status(:ok)
+      #expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to "/posts/index"
+      #get posts_index_path
+      #expect(response).to have_http_status(:ok)
     end
   end
 
@@ -38,6 +39,47 @@ RSpec.describe "SignUp & Logins", type: :request do
       expect(user02).to be_valid
       get "/login"
       expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "Unsuccessful Edit" do
+    let(:user02) { FactoryBot.create(:user02) }
+    it "responds successfully" do
+      post login_path, params: {
+        email: user02.email,
+        password: user02.password,
+      }
+      expect(user02).to be_valid
+      expect(response).to have_http_status(:redirect)
+      get "/users/#{user02.id}/edit"
+      expect(response).to have_http_status(:ok)
+      post "/users/#{user02.id}/update", params: {
+                            email: "fake@invalid",
+                            password: "",
+                          }
+      get "/users/#{user02.id}/edit"
+      expect(response).to have_http_status "200"
+    end
+  end
+
+  describe "Successful Edit" do
+    let(:user02) { FactoryBot.create(:user02) }
+    it "responds successfully" do
+      post login_path, params: {
+        email: user02.email,
+        password: user02.password,
+      }
+      expect(user02).to be_valid
+      expect(response).to have_http_status(:redirect)
+      get "/users/#{user02.id}/edit"
+      expect(response).to have_http_status(:ok)
+      post "/users/#{user02.id}/update", params: {
+                            name:  "updating",
+                            email: "updating@example.com",
+                          }
+      expect(response).to redirect_to "/users/#{user02.id}"
+      edited_user = User.find(user02.id)
+      expect(user02.name).not_to eq edited_user.name
     end
   end
 
