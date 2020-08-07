38af670e01739470b491140094abb0f2b2600887
mkakiz <masahiro.kakizaki@gmail.com>
2020-08-07 07:46:18 -0400

    Vue.js text count

--- app/assets/javascripts/vue.js
+++ app/assets/javascripts/vue.js
@@ -2,10 +2,8 @@ window.onload = function () {
   var app = new Vue({
     el: "#app",
     data: {
-      message: '<h2>家にあるもので人と繋がれる</h2><p>タダカシは無料で貸し借りできるマッチングサイトです。<br>あなたが今使ってないものを投稿してみよう!</p>'
-    },
-    data: {
-      textCount: ""
+      message: '<h2>家にあるもので人と繋がれる</h2><p>タダカシは無料で貸し借りできるマッチングサイトです。<br>あなたが今使ってないものを投稿してみよう!</p>',
+      postText: ''
     }
   });
 }
