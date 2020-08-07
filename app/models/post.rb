cf5f593f11ec9e018864fe62a1987d1f9cab36e6
lunasock <59585633+lunasock@users.noreply.github.com>
2020-08-07 16:25:37 +0900

    get varidate max length

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
 
-  validates :content, {presence: true, length: {maximum: 140}}
+  validates :content, {presence: true, length: {maximum: CONTENT_MAX_LENGTH = 140}}
   validates :user_id, {presence: true}
 
   #def user
