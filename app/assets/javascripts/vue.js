97fc372d3d0ef4a89d8c42672235a5f7a4d6fa75
mkakiz <masahiro.kakizaki@gmail.com>
2020-08-10 21:25:52 -0400

    post_new.js

--- app/assets/javascripts/vue.js
+++ app/assets/javascripts/vue.js
@@ -1,9 +1,7 @@
-window.onload = function () {
-  var app = new Vue({
-    el: "#app",
-    data: {
-      message: '<h2>家にあるもので人と繋がれる</h2><p>タダカシは無料で貸し借りできるマッチングサイトです。<br>あなたが今使ってないものを投稿してみよう!</p>',
-      postText: ''
-    }
-  });
-}
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: '<h2>家にあるもので人と繋がれる</h2><p>タダカシは無料で貸し借りできるマッチングサイトです。<br>あなたが今使ってないものを投稿してみよう!</p>',
+  }
+});
+
