cd883cd5ed58ea1707f4d171c12897d606386633
mkakiz <masahiro.kakizaki@gmail.com>
2020-08-14 00:57:13 -0400

    vue.js slide text

---
+++ app/assets/javascripts/top_slides.js
@@ -0,0 +1,13 @@
+const top_slides = function() {
+  var app = new Vue({
+    el: '#top_slides',
+    data: {
+      slides: 3
+    },
+    components: {
+      'carousel-3d': Carousel3d.Carousel3d,
+      'slide': Carousel3d.Slide,
+      speed: 1000,
+    }
+  });
+}
