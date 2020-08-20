75965982498d9653895f713c1709d8508d62ba72
mkakiz <masahiro.kakizaki@gmail.com>
2020-08-19 23:32:07 -0400

    Initialize repository

--- app/assets/javascripts/top_slides.js
+++ app/assets/javascripts/top_slides.js
@@ -2,11 +2,28 @@ const top_slides = function() {
   var app = new Vue({
     el: '#top_slides',
     data: {
-      slides: 3
+      slideData: [
+        { text: 'slide text1', image: 'friends.jpg' },
+        { text: 'slide text2', image: 'friends.jpg' },
+        { text: 'slide text3', image: 'friends.jpg' }
+      ],
+      descriptions: [
+        'current',
+        '',
+        ''
+      ]
     },
     components: {
       'carousel-3d': Carousel3d.Carousel3d,
       'slide': Carousel3d.Slide,
+    },
+    methods: {
+      onAfterSlideChange(index){
+        this.descriptions.forEach((obj, objIndex) => {
+          index == objIndex ? this.$set(this.descriptions, objIndex, 'current')
+          : this.$set(this.descriptions, objIndex, '');
+        });
+      }
     }
   });
 }
