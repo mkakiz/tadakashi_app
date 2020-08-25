395d0f6ca15fc6719305b4dcbfaf1608f428d102
mkakiz <masahiro.kakizaki@gmail.com>
2020-08-25 07:46:38 -0400

    3 slides pic text done

--- app/assets/javascripts/top_slides.js
+++ app/assets/javascripts/top_slides.js
@@ -2,31 +2,43 @@ const top_slides = function() {
   var app = new Vue({
     el: '#top_slides',
     data: {
-      //slides: 3
-      slides: [
-        { text: '欲しい物を探して', image: 'finditem.jpg' },
-        { text: 'メッセージして', image: 'messaging.jpg' },
-        { text: '貸してもらおう！', image: 'getitem.jpg' }
+    //  slides: 3,
+    //  slideData: [
+    //    { text: 'Find', image: 'finditem.jpg' },
+    //    { text: 'Message', image: 'messaging.jpg' },
+    //    { text: 'Borrow', image: 'getitem.jpg' }
+    //  ],
+    //},
+    slideData: [
+      {text: 'Find', src: 'finditem.jpg'},
+      {text: '', src: 'messaging.jpg'},
+      {text: '', src: 'getitem.jpg'}
       ],
-      descriptions: [
-        'current',
-        '',
-        ''
+      texts: [
+        'Find',
+        'Message',
+        'Borrow'
       ]
     },
     components: {
       'carousel-3d': Carousel3d.Carousel3d,
       'slide': Carousel3d.Slide,
     },
+    //methods: {
+    //  onAfterSlideChange(index){
+    //    this.slideData.forEach((obj, objIndex) => {
+    //      index == objIndex ? this.$set(this.slide, objIndex, slide.text)
+    //      : this.$set(this.slide, objIndex, '');
+    //    //this.descriptions.forEach((obj, objIndex) => {
+    //    //  index == objIndex ? this.$set(this.descriptions, objIndex, 'current')
+    //    //  : this.$set(this.descriptions, objIndex, '');
+    //    });
+    //  }
+    //}
     methods: {
-      onAfterSlideChange(index){
-        this.descriptions.forEach((obj, objIndex) => {
-          index == objIndex ? this.$set(this.descriptions, objIndex, 'current')
-          : this.$set(this.descriptions, objIndex, '');
-        //this.descriptions.forEach((obj, objIndex) => {
-        //  index == objIndex ? this.$set(this.descriptions, objIndex, 'current')
-        //  : this.$set(this.descriptions, objIndex, '');
-        });
+      onAfterSlideChange(currentIndex) {
+        this.slideData = this.slideData.map(e => ({ src: e.src}))
+        this.slideData[currentIndex]['text'] = this.texts[currentIndex]
       }
     }
   });
