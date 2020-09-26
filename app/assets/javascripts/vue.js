dc4e146ac9e36b741665bf9a4f45a5c1a37b2455
mkakiz <masahiro.kakizaki@gmail.com>
2020-09-26 16:05:56 -0400

    button fixed

--- app/assets/javascripts/vue.js
+++ app/assets/javascripts/vue.js
@@ -2,7 +2,7 @@ const run_app_vue = function() {
   var app = new Vue({
     el: "#app",
     data: {
-      message: '<h2>家にあるもので人と繋がれる</h2><p>タダカシは無料で貸し借りできるマッチングサイトです。<br>あなたが今使ってないものを投稿してみよう!</p>',
+      message: '<h2>家にあるもので人と繋がれる</h2>',
     }
   });
 }
